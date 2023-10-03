2f9370c84a04dca8eccaf45baa0420dbb2b76c05
Disza Jatnika <diszaid@gmail.com>
2023-10-03 18:34:30 +0700

    first commit

---
+++ beranda.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="beranda.css"> 
+    <title>Halaman Beranda</title>
+</head>
+<body>
+    <header>
+        <div class="menu">
+            <a href="#section1">Beranda</a>
+            <a href="#section2">Profil</a>
+            <a href="#section3">Kontak</a>
+            <a href="index.php" id="logout-link">Logout</a>
+        </div>
+    </header>
+
+    <div class="home-container">
+        <h2>Selamat Datang di Halaman Beranda!</h2>
+        <p>Ini adalah halaman beranda setelah login.</p>
+
+        <a href="index.php">Logout</a>
+    </div>
+
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="login-container">
+        <form action="beranda.php">
+            <h2>Login</h2>
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username" placeholder="Masukkan username" required>
+            
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" placeholder="Masukkan password" required>
+
+            <button type="submit">Login</button>
+        </form>
+    </div>
+</body>
+</html>
+
+
+
